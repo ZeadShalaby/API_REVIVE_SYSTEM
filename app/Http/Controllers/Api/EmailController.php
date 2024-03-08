706aadfc19b1f2d8ea3df0f47db3a4d2e38912d6
Zead_Shalaby <you@example.com>
2024-03-08 19:12:39 +0200

    send mail for all users you want

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use Carbon\Carbon;
+use Exception;
+use Validator;
 use App\Events\MailCode;
+use App\Mail\ReviveMail;
 use App\Mail\Emailmailer;
 use Illuminate\Http\Request;
 use App\Traits\ResponseTrait;
+use App\Traits\MethodconTrait;
 use App\Mail\ErrorMachinemailer;
+use App\Traits\Requests\TestAuth;
 use App\Traits\Requests\TestMail;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
@@ -15,7 +20,7 @@ use Illuminate\Support\Facades\Mail;
 class EmailController extends Controller
 {
     //
-    use ResponseTrait , TestMail;
+    use ResponseTrait , TestMail ,TestAuth ,MethodconTrait;
     
     // todo send mail when i want forget pass he send code into mail //
     public function sendmail(Request $request){
@@ -36,5 +41,26 @@ class EmailController extends Controller
         return $this->returnSuccessMessage("Send Successfully sir : ".$info->username." lock in inbox");
     }
 
+    // todo Send Mail For all users or only Owner or Admin or customer in App //
+    public function SendAll(Request $request){
+        // ! rules 
+        $rules = $this->rulestype();
+        // ! valditaion
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            $code = $this->returnCodeAccordingToInput($validator);
+            return $this->returnValidationError($code,$validator);
+        }
+
+        $type = $this->checkTypeUsers($request->type);
+        $users = $this->checkussersmail($type);
+        foreach ($users as $info) {
+        $date = Carbon::now();
+        $info->date = $date;
+        Mail::to($info->gmail)->send(new ReviveMail($info));
+        }
+        return $this->returnSuccessMessage("Send Successfully sir : Admin Revive ");
+
+    }
 
 }
--- app/Traits/Requests/TestMail.php
+++ app/Traits/Requests/TestMail.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Traits\Requests;
 
+use App\Models\Role;
 use App\Models\User;
 
 trait TestMail
 
 { 
-
+    // todo check gmail and return code to do forget pass //
     protected function checkmail($gmail){
         $userid = User::where("gmail",$gmail)->value('id');
         $info = User::find($userid);
@@ -14,6 +15,16 @@ trait TestMail
             $info->code = random_int(100000, 999999);
             return  $info;
         }else {return null;}
+    }
+
+    // todo send mail for every users or every owner or admin or customer //
+    protected function checkussersmail($type){
+        if($type == ""){
+            $user = User::where("role","!=",Role::ADMIN)->get();
+            return $user;}
+        else{
+            $user = User::where("role",$type)->get();
+            return $user;}
     }
 
 
