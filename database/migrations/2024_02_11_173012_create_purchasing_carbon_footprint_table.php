c4dc9ff4edd3226e719b2979400e41d56d8f4cf3
Zead_Shalaby <you@example.com>
2024-03-12 17:11:57 +0200

    last verison database

---
+++ database/migrations/2024_02_11_173012_create_purchasing_carbon_footprint_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchasing_carbon_footprint', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('seller_id')->unsigned()->nullable();
+            $table->index('seller_id');
+            $table->bigInteger('buyer_id')->unsigned()->nullable();
+            $table->index('buyer_id');
+            $table->bigInteger('carbon_footprint')->unsigned()->nullable();
+            $table->index('carbon_footprint');
+            $table->bigInteger('expire')->unsigned()->default(1);
+            $table->index('expire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchasing_carbon_footprint');
+    }
+};
